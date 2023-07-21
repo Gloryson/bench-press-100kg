bb36ba068002bb5773e342de3b81ec884539781e
Vlad Bolkunov <gloryson@mail.ru>
2023-07-21 11:16:49 +0200

    feat: added training programm

--- database/training.ts
+++ database/training.ts
@@ -1,7 +1,424 @@
 
 
 export const training = [
+
+  // 1 week, index 0, 1 entry point (35x12 or 40x10 or 45x8 or 50x6)
+
   [
-    {},{},{}
-  ]
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 30 kg - 4 sets - 7 reps',
+      'Bench press: 40 kg - 3 sets - 5 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 25 kg - 5 sets - 8 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 30 kg - 4 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 35 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 40 kg - 4 sets - 5 reps',
+      'Bench press: 45 kg - 2 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 2 week, index 1
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 35 kg - 4 sets - 6 reps',
+      'Bench press: 45 kg - 3 sets - 4 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 30 kg - 5 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 35 kg - 4 sets - 5 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 3 sets - 5 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 45 kg - 3 sets - 4 reps',
+      'Bench press: 50 kg - 2 sets - 3 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 3 week, index 2, 2 entry point (40x12 or 45x10 or 50x8 or 55x6)
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 4 sets - 7 reps',
+      'Bench press: 45 kg - 3 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 35 kg - 5 sets - 8 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 40 kg - 4 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 45 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 50 kg - 4 sets - 5 reps',
+      'Bench press: 55 kg - 2 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 4 week, index 3
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 4 sets - 4 reps',
+      'Bench press: 55 kg - 3 sets - 3 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 5 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 45 kg - 4 sets - 5 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 3 sets - 5 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 50 kg - 3 sets - 4 reps',
+      'Bench press: 60 kg - 2 sets - 3 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 5 week, index 4
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 4 sets - 7 reps',
+      'Bench press: 45 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 35 kg - 3 sets - 8 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 40 kg - 3 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 50 kg - 4 sets - 5 reps',
+      'Bench press: 60 kg - 3 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 6 week, index 5, 3 entry point (50x12 or 55x10 or 60x8 or 65x6)
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 4 sets - 5 reps',
+      'Bench press: 55 kg - 3 sets - 4 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 4 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 45 kg - 4 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 55 kg - 3 sets - 5 reps',
+      'Bench press: 60 kg - 2 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 7 week, index 6
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 55 kg - 3 sets - 4 reps',
+      'Bench press: 60 kg - 3 sets - 3 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 45 kg - 5 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 50 kg - 4 sets - 5 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 45 kg - 3 sets - 5 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 55 kg - 3 sets - 4 reps',
+      'Bench press: 65 kg - 2 sets - 3 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 8 week, index 7
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 4 sets - 7 reps',
+      'Bench press: 45 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 35 kg - 4 sets - 8 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 40 kg - 4 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 45 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 50 kg - 4 sets - 5 reps',
+      'Bench press: 55 kg - 4 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 9 week, index 8, 4 entry point (60x12 or 65x10 or 70x8 or 75x6)
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 55 kg - 3 sets - 5 reps',
+      'Bench press: 60 kg - 3 sets - 4 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 3 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 45 kg - 3 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 60 kg - 3 sets - 5 reps',
+      'Bench press: 65 kg - 2 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 10 week, index 9
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 60 kg - 3 sets - 4 reps',
+      'Bench press: 65 kg - 3 sets - 3 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 4 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 55 kg - 4 sets - 5 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 60 kg - 3 sets - 5 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 65 kg - 2 sets - 4 reps',
+      'Bench press: 70 kg - 2 sets - 3 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 11 week, index 10
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 45 kg - 4 sets - 7 reps',
+      'Bench press: 50 kg - 3 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 40 kg - 4 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 45 kg - 4 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 4 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 55 kg - 4 sets - 5 reps',
+      'Bench press: 60 kg - 3 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 12 week, index 11, 5 entry point (70x12 or 75x10 or 80x8 or 85x6)
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 60 kg - 3 sets - 5 reps',
+      'Bench press: 65 kg - 2 sets - 4 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 45 kg - 3 sets - 7 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 50 kg - 2 sets - 6 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 55 kg - 3 sets - 6 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 65 kg - 3 sets - 5 reps',
+      'Bench press: 70 kg - 2 sets - 4 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 13 week, index 12
+
+  [
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 70 kg - 3 sets - 4 reps',
+      'Bench press: 75 kg - 2 sets - 3 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ],
+    [
+      'Warm-up',
+      'Hyperextension: 3 sets - 12 reps',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 50 kg - 3 sets - 6 reps',
+      'Pull-ups: 3 sets - 5 reps',
+      'Bench press: 60 kg - 3 sets - 5 reps'
+    ],
+    [
+      'Warm-up',
+      'Bench press: 20 kg - 1 set - 12 reps',
+      'Bench press: 62.5 kg - 3 sets - 5 reps',
+      'Back squats: 3 sets - 8 reps',
+      'Bench press: 70 kg - 2 sets - 4 reps',
+      'Bench press: 77.5 kg - 2 sets - 3 reps',
+      'Any triceps exercise: 3 sets - 10 reps',
+      'Any biceps exercise: 3 sets - 10 reps'
+    ]
+  ],
+
+  // 14 week, index 13
+
+  
 ]
